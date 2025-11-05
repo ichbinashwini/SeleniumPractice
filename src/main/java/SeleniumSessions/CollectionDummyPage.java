4d50a81511494350b3d40d6de494d1ed6a71c590
AshleyN <ashwinidn0143@gmail.com>
2025-11-05 15:19:17 +0530

    This will add the locator as per review comment of TL

--- src/main/java/SeleniumSessions/CollectionDummyPage.java
+++ src/main/java/SeleniumSessions/CollectionDummyPage.java
@@ -1,12 +1,13 @@
 package SeleniumSessions;
 
+import org.openqa.selenium.By;
+
 public class CollectionDummyPage {
 
 	public static void main(String[] args) {
 
-		
 		String variable = "This is dummy page";
-		
-	}
+		By button = By.xpath("//div[@name='Submit']");
 
+	}
 }
